bcc59536013fce55fd34e724f9038c8284a0927a
Anmol <anmol@qdesq.com>
2023-09-29 04:44:06 +0530

    Task 1

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TextInput } from 'react-native';
 
 export default function App() {
+  const [value, onChangeText] = 	React.useState('');  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{'marginBottom':20}}>Basic App</Text>
+      <TextInput style={styles.inputStyle} placeholder="Type something"
+	    onChangeText={text => onChangeText(text)}
+      clearTextOnFocus="true"
+      value={value}/>
+      <Text>{value}</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -17,4 +24,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputStyle : {
+    width: 300,
+    height: 40,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom:10
+  }
 });
